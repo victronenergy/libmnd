dc4404f543cfb004122414f851f1d0bbadebd741
Malcolm Herring <malcolm.herring@btinternet.com>
2017-12-21 13:55:25 +0000

    restructure

--- inc/mnd/n0183.h
+++ inc/mnd/n0183.h
@@ -15,21 +15,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-// Function to convert NMEA.0183 sentences to text
-
 extern char* translateN0183(char* ln, char* d0183);
-
-// ln is sentence text string
-// d0183 points to char array reserved for conversion string
-// Return value: pointer to d0183 if conversion performed, NULL otherwise
-
-// Function to convert Tracked Target encapsulated data to text
-
-extern char* translateTTD(uint8_t* buf, int len, char* decttd);
-
-// buf points to encapsulated data
-// len is number of valid bytes in buf
-// decttd points to char array reserved for conversion string
-// Return value: pointer to decttd if conversion performed, NULL otherwise
+extern char* translateTTD(uint8_t* buf, int len, char* trttd);
 
 #endif /* n0183_h */
